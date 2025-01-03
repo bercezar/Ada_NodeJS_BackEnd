6b1e969c2bce937dd7f8a3811609387f6850ca59
Bernardo Cezar <b.cezar1812@gmail.com>
2025-01-03 14:02:43 -0300

    herança

--- aula-15/02-oop.ts
+++ aula-15/02-oop.ts
@@ -7,6 +7,21 @@ interface EstabelecimentoInterface {
   diminuirFilaDeEspera: () => void;
 }
 
+interface Remedio extends Produto {
+  receitaObrigatoria?: boolean;
+}
+
+interface ReceitaInterface {
+  remedios: string[];
+  crm: string;
+}
+interface FarmaciaInterface extends EstabelecimentoInterface {
+  compraRemedioPrescrito: (
+    receita: ReceitaInterface,
+    produtosAcomprar: string[]
+  ) => void;
+}
+
 class Estabelecimento implements EstabelecimentoInterface {
   protected _filaDeEspera = 10;
 
@@ -52,13 +67,48 @@ const supermercado = new Estabelecimento(
   8
 );
 
-class Farmacia extends Estabelecimento implements EstabelecimentoInterface {
+class Farmacia extends Estabelecimento implements FarmaciaInterface {
   constructor(
     public endereco: string,
     public setor: string,
-    protected produtos: Produto[],
+    protected produtos: Remedio[],
     filaDeEspera?: number
   ) {
     super(endereco, setor, produtos, filaDeEspera);
   }
+
+  public compraRemedioPrescrito(
+    receita: ReceitaInterface,
+    produtosAcomprar: string[]
+  ) {
+    const remediosDisponiveis = this.retornaProdutos().filter((produto) =>
+      produtosAcomprar.includes(produto)
+    );
+
+    if (remediosDisponiveis.length === 0)
+      console.log("Infelizmente não temos os remédios em estoque");
+
+    const remediosComReceita = remediosDisponiveis.filter((produto) =>
+      receita.remedios.includes(produto)
+    );
+
+    console.log({ remediosDisponiveis });
+    console.log({ remediosComReceita });
+  }
 }
+
+const farmacia = new Farmacia("Rua Dr dos remédios - N°1029", "Farmaceutico", [
+  { nome: "Dorflex", valor: 4.99, receitaObrigatoria: false },
+  { nome: "RivoTril", valor: 10.9, receitaObrigatoria: true },
+  { nome: "Vitamina C", valor: 5.5, receitaObrigatoria: false },
+  { nome: "Dipirona", valor: 4.34, receitaObrigatoria: false },
+]);
+
+farmacia.retornaProdutos();
+farmacia.compraRemedioPrescrito(
+  {
+    remedios: ["Vitamina C", "Dipirona", "B12"],
+    crm: "6519-CRMRJ",
+  },
+  ["Dipirona", "B12"]
+);
